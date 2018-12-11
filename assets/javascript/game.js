d59827832bb21bba090963e68906aa4bf59e03b6
Kevin Patel <kpatel1209@gmail.com>
2018-12-11 12:03:21 -0500

    logic is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,94 +1,59 @@
-$(document).ready(function() {
+// Global Variables
+let wins = 0;
+let losses = 0;
+let counter = 0;
+let computerChoice = "";
+let imagesCrystals = ["./assets/images/crystal1.png", "./assets/images/crystal2.png", "./assets/images/crystal3.png", "./assets/images/crystal4.png"];
 
-    let computerChoice = Math.floor(Math.random() * 102) + 19;
-        console.log("computerChoice: "+ computerChoice);
-        $(".randomNumber").html("Get to this number: " + computerChoice);
-
-    let crystalOne = Math.floor(Math.random() * 12) + 1;
-        console.log("Crystal 1: " + crystalOne);
-        $("#crystal1").html("<img src=" + "assets/images/crystal1.jpg" + " value=" + crystalOne + ">");
-
-    let crystalTwo = Math.floor(Math.random() * 12) + 1;
-        console.log("Crystal 2: " + crystalTwo);
-        $("#crystal2").html("<img src=" + "assets/images/crystal2.jpg" + " value=" + crystalTwo + ">");
-
-    let crystalThree = Math.floor(Math.random() * 12) + 1;
-        console.log("Crystal 3: " + crystalThree);
-        $("#crystal3").html("<img src=" + "assets/images/crystal3.jpg" + " value=" + crystalThree + ">");
-
-    let crystalFour = Math.floor(Math.random() * 12) + 1;
-        console.log("Crystal 4: " + crystalFour);
-        $("#crystal4").html("<img src=" + "assets/images/crystal4.jpg" + " value=" + crystalFour + ">");
-
-    let wins = 0;
-        console.log("Wins: " + wins);
-
-    let losses = 0;
-        console.log("Losses: " + losses);
-    
-    let score = 0;
-        console.log("Your total score is: " + score);
-
-    function reset () {
+    // Functions
+    function randomComputerChoice () {
         computerChoice = Math.floor(Math.random() * 102) + 19;
-            console.log("computerChoice" + computerChoice);
-        $(".randomNumber").html(computerChoice);
+    }
 
-        score = 0;
-        $(".scoreView").html(score);
+    function resetCrystals () {
+        for (let i = 0; i < imagesCrystals.length; i++) {
+            let crystal = $("<img>");
+            crystal.addClass("crystal");
+            crystal.attr("src", imagesCrystals[i]);
+            crystal.attr("value", (Math.floor(Math.random() * 12) + 1));
+            $(".crystal-pics").append(crystal); 
+        }
+    }
 
-        crystalOne = Math.floor(Math.random () * 12) + 1;
-            console.log("Crystal 1: " + crystalOne);
-        $("#crystal1").html("<img src=" + "assets/images/crystal1.jpg" + " value=" + crystalOne + ">");
+    function resetGame() {
+        $(".randomNumber").html("<p>Get to this number: " + computerChoice);
+        $(".counterWinsLosses").html("<p>Wins: " + wins + "<p>" + "<p>Losses: " + losses + "<p>");
+        $(".scoreUser").html(counter);
+        $(".crystal-pics").empty();
+    }
 
-        
-        crystalTwo = Math.floor(Math.random () * 12) + 1;
-            console.log("Crystal 2: " + crystalTwo);
-        $("#crystal2").html("<img src=" + "assets/images/crystal2.jpg" + " value=" + crystalTwo + ">");
+    function totalReset () {
+        randomComputerChoice ();
+        counter = 0;
+        resetGame ();
+        resetCrystals ();
+    }
 
+    // Run Code
+    randomComputerChoice ();
+    resetGame ();
+    resetCrystals ();
 
-        crystalThree = Math.floor(Math.random () * 12) + 1;
-            console.log("Crystal 3: " + crystalThree);
-        $("#crystal3").html("<img src=" + "assets/images/crystal3.jpg" + " value=" + crystalThree + ">");
+    // Click Function
+    $(document).on("click", ".crystal", crystalClick);
 
-
-        crystalFour = Math.floor(Math.random () * 12) + 1;
-            console.log("Crystal 4: " + crystalFour);
-        $("#crystal4").html("<img src=" + "assets/images/crystal4.jpg" + " value=" + crystalFour + ">");
-
+    function crystalClick () {
+        counter += parseInt($(this).attr("value"));
+        $(".scoreUser").html(counter);
+        if (counter === computerChoice) {
+            wins++;
+            alert("YOU WIN!")
+            totalReset();
+        } else if (counter > computerChoice) {
+            losses++;
+            alert("YOU LOSE!")
+            totalReset();
+        };
+    };
     
     
-    
-    
-    
-    }   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
